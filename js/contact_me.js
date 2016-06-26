eb91c9e2a064830072b1fca27aa159dd0d2c7a9d
Darragh Jones! <darragh.jones@gmail.com>
2016-06-26 23:43:51 +0100

    fix form

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,6 +26,7 @@ $(function() {
                     email: email,
                     message: message
                 },
+                dataType: 'json',
                 cache: false,
                 success: function() {
                     // Success message
@@ -33,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you for getting in touch. I look forward to discussing your project :)</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
